3cf35e6b16f0a06d256dc4b7fab06c0cbe62250a
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-07-23 03:13:44 +0200

    Add files via upload

--- fonctions/gestion_bdd.py
+++ fonctions/gestion_bdd.py
@@ -11,7 +11,7 @@ import os
 
 DB = os.environ.get('API_SQL')
 engine = create_engine(DB, echo=False)
-conn = engine.connect()
+
 
 
 def lire_bdd(nom_table, format:str="df"):
@@ -24,12 +24,14 @@ def lire_bdd(nom_table, format:str="df"):
     format: :class:`str`
             Choix entre 'dict' ou 'df'
     """
-    
+    conn = engine.connect()
     df = pd.read_sql(f'SELECT * FROM {nom_table}', con=conn, index_col='index')
     df = df.transpose()
     if format == "dict":
         df = df.to_dict()
+    conn.close()
     return df
+    
 
 
 def sauvegarde_bdd(df, nom_table, methode_save='replace'):
@@ -44,13 +46,16 @@ def sauvegarde_bdd(df, nom_table, methode_save='replace'):
     method_save: :class:`str`
             Si la table existe déjà, choix entre "append" pour insérer des nouvelles valeurs ou "replace" pour supprimer la table existante et la remplacer
     """
+    conn = engine.connect()
     if not isinstance(df, pd.DataFrame): # si la variable envoyée n'est pas un dataframe, on l'a met au format dataframe
         df = pd.DataFrame(df)
         df = df.transpose()
     df.to_sql(nom_table, con=conn, if_exists=methode_save, index=True, method='multi', dtype={'Score' : Float(), 'serie' : BigInteger()})
-    
+    conn.close()
     
 def supprimer_bdd(nom_table):
+    conn = engine.connect()
     sql = text(f'DROP TABLE IF EXISTS {nom_table}')
     conn.execute(sql)
+    conn.close()
     
