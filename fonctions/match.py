dc0d762965e41865243092629f64262e99fdb954
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-10-30 17:29:53 +0100

    S12 constant
    En attendant la s13 ...

--- fonctions/match.py
+++ fonctions/match.py
@@ -175,7 +175,8 @@ class matchlol():
         self.match_detail_participants = self.match_detail['info']['participants'][self.thisId]
         self.match_detail_challenges = self.match_detail_participants['challenges']
         self.thisPosition = self.match_detail_participants['teamPosition']
-        self.season = int(self.match_detail['info']['gameVersion'][0:2])
+        # self.season = int(self.match_detail['info']['gameVersion'][0:2])
+        self.season = 12 # a modifier quand s13
         
         if (str(self.thisPosition) == "MIDDLE"):
             self.thisPosition = "MID"
