85faeee258a128783ec6f89a73c0bda15cd15a3c
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-11-01 02:23:01 +0100

    Add files via upload

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -8,19 +8,21 @@ import os
 from TwitterAPI import TwitterAPI
 
 
-api = TwitterAPI(os.environ['API_TWITTER'],
-                 os.environ['SECRET_TWITTER'],
-                 os.environ['ACCESS_TWITTER'],
-                 os.environ['ACCESS_SECRET_TWITTER'],
+api = TwitterAPI(os.environ.get('API_TWITTER'),
+                 os.environ.get('SECRET_TWITTER'),
+                 os.environ.get('ACCESS_TWITTER'),
+                 os.environ.get('ACCESS_SECRET_TWITTER'),
                  api_version='2')
 
 def get_user_id(user:str):
+    print('user')
     user_r = api.request(f'users/by/username/:{user}')
     user_r = user_r.json()
     user_id = user_r['data']['id']
     return user_id
 
 def get_tweet(user_id:str, num_tweet:int=0):
+    print('get_tweet')
     tweets = api.request(f'users/:{user_id}/tweets', {'max_results' : 5})
     id_tweet = tweets.json()['data'][num_tweet]['id']
     contenu_tweet = tweets.json()['data'][0]['text']
@@ -38,13 +40,17 @@ class Twitter(commands.Cog):
                        options=[create_option(name="pseudo", description= "pseudo twitter", option_type=3, required=True)])
     async def last_tweet(self, ctx, pseudo:str):
         
+        print('last_tweet')
         user_id = get_user_id(pseudo)
         ctx.defer(hidden=False)
         
+        print('a')
         id_tweet, msg_tweet = get_tweet(user_id)
         
+        print('b')
         url_tweet = f'https://twitter.com/{pseudo}/status/{id_tweet}'
         
+        print('c')
         ctx.send(url_tweet)
 
 
