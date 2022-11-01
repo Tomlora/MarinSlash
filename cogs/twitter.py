3b8b0f1ae708289dbb411b3194b73b4585169f24
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-11-01 02:31:59 +0100

    Add files via upload

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -4,8 +4,10 @@ from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_components import *
 from discord_slash.utils.manage_commands import create_option, create_choice
 import os
+from fonctions.gestion_bdd import get_data_bdd, requete_perso_bdd
 
 from TwitterAPI import TwitterAPI
+import main
 
 
 api = TwitterAPI(os.environ.get('API_TWITTER'),
@@ -15,17 +17,17 @@ api = TwitterAPI(os.environ.get('API_TWITTER'),
                  api_version='2')
 
 def get_user_id(user:str):
-    print('user')
+
     user_r = api.request(f'users/by/username/:{user}')
     user_r = user_r.json()
     user_id = user_r['data']['id']
     return user_id
 
 def get_tweet(user_id:str, num_tweet:int=0):
-    print('get_tweet')
+
     tweets = api.request(f'users/:{user_id}/tweets', {'max_results' : 5})
     id_tweet = tweets.json()['data'][num_tweet]['id']
-    contenu_tweet = tweets.json()['data'][0]['text']
+    contenu_tweet = tweets.json()['data'][num_tweet]['text']
     
     return id_tweet, contenu_tweet
 
@@ -40,20 +42,54 @@ class Twitter(commands.Cog):
                        options=[create_option(name="pseudo", description= "pseudo twitter", option_type=3, required=True)])
     async def last_tweet(self, ctx, pseudo:str):
         
-        print('last_tweet')
+
         user_id = get_user_id(pseudo)
         ctx.defer(hidden=False)
         
-        print('a')
+
         id_tweet, msg_tweet = get_tweet(user_id)
         
-        print('b')
+ 
         url_tweet = f'https://twitter.com/{pseudo}/status/{id_tweet}'
         
-        print('c')
+        print(url_tweet)
         ctx.send(url_tweet)
+        
+    @tasks.loop(minutes=1, count=None )
+    async def twitter_suivi(self):
+        try:
+            channel_tracklol = self.bot.get_channel(int(main.chan_tracklol)) 
+                
+            df_twitter =  get_data_bdd('Select * from twitter')
+            df_twitter = df_twitter.mappings().all()
+            
+            for twitter in df_twitter:
+                # info de la BDD
+                user = twitter['twitter'] # user
+                id_last_msg = twitter['id_last_msg_twitter'] # id dernier msg
+                
+                user_r = api.request(f'users/by/username/:{user}') # récupère l'id twitter avec l'user
+                user_r = user_r.json() # format json
+                user_id = user_r['data']['id'] # on récupère l'id
+                
+                # now on cherche les tweets
+                tweets = api.request(f'users/:{user_id}/tweets', {'max_results' : 5})
+                
+                # id du dernier tweet
+                id_tweet = tweets.json()['data'][0]['id']
+                
+                # contenu du dernier tweet
+                contenu_tweet = tweets.json()['data'][0]['text']
+                print(contenu_tweet)
+                
+                if ('sources' in contenu_tweet.lower() or 'source' in contenu_tweet.lower()) and (str(id_tweet) != str(id_last_msg)): # info officiel
+                    url_tweet = f'twitter.com/{user}/status/{id_tweet}'
+                    channel_tracklol.send(url_tweet)
+                    requete_perso_bdd('UPDATE twitter SET id_last_msg_twitter = :id_last_msg WHERE id_twitter = :id_twitter', {'id_last_msg' : id_tweet,
+                                                                                                                            'id_twitter' : user_id} )
 
-
+        except:
+            print('Erreur de détection du channel')
 
 
 
