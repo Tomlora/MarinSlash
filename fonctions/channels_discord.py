00ef6a9316673093b03829ee1d977a8a714fc938
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-11-15 22:05:46 +0100

    Add files via upload

--- fonctions/channels_discord.py
+++ fonctions/channels_discord.py
@@ -2,32 +2,15 @@ from fonctions.gestion_bdd import lire_bdd_perso, requete_perso_bdd
 
 class chan_discord():
 
-    def __init__(self, server_id:int, bot=None):
-        self.bot_discord = bot
+    def __init__(self, server_id:int):
         self.server_id = server_id
-        # si le serveur n'est pas dans la liste :  
         
-        if self.bot_discord != None:
-        # on vérifie que le serveur est enregistré
-            data = lire_bdd_perso(f'SELECT server_id from channels_discord', index_col='server_id').transpose()
-            
-            self.text_channel_list = []
-            self.guild = self.bot_discord.get_guild(self.server_id)
-            for channel in self.guild.text_channels:
-                self.text_channel_list.append(channel.id)
-            
-            # on vérifie que le serveur est enregistré
-            data = lire_bdd_perso(f'SELECT server_id from channels_discord', index_col='server_id').transpose()
-            
-            if not int(server_id) in data.index:
-                    requete_perso_bdd(f'''INSERT INTO public.channels_discord(
-                    server_id, id_owner, id_owner2, chan_pm, chan_tracklol, chan_accueil, chan_twitch, chan_lol, chan_tft, chan_lol_others, role_admin)
-                    VALUES (:server_id, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan);''',
-                {'server_id' : server_id.id, 'chan' : self.text_channel_list[0]})
-             
-            
+        # si le serveur n'est pas dans la liste :
+        self.data = lire_bdd_perso(f'SELECT server_id from channels_discord', index_col='server_id').transpose() 
         
-        
+        if not int(self.server_id) in self.data.index:
+            self.verif_server() 
+                
         self.dict_channel = lire_bdd_perso('Select * from channels_discord where server_id = %(server_id)s', index_col='server_id', format='dict', params={'server_id' : self.server_id} )
         
         self.dict_channel = self.dict_channel[self.server_id]
@@ -43,5 +26,21 @@ class chan_discord():
         self.tft = self.dict_channel['chan_tft']
         self.lol_others = self.dict_channel['chan_lol_others']
         self.role_admin = self.dict_channel['role_admin']
+        
+    def verif_server(self):
+
+            
+        self.text_channel_list = []
+        self.guild = self.bot_discord.get_guild(self.server_id)
+        for channel in self.guild.text_channels:
+            self.text_channel_list.append(channel.id)
+            
+            # on vérifie que le serveur est enregistré
+        
+        requete_perso_bdd(f'''INSERT INTO public.channels_discord(
+                    server_id, id_owner, id_owner2, chan_pm, chan_tracklol, chan_accueil, chan_twitch, chan_lol, chan_tft, chan_lol_others, role_admin)
+                    VALUES (:server_id, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan);''',
+                {'server_id' : self.server_id, 'chan' : self.text_channel_list[0]})
+             
 
         
