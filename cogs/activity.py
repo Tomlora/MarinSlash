be88573eb8fc69740f0af5c837addce884bab41c
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-11-15 21:51:55 +0100

    Add files via upload

--- cogs/activity.py
+++ cogs/activity.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from fonctions.date import heure_actuelle
+from fonctions.gestion_bdd import lire_bdd_perso, requete_perso_bdd
 
 import main
 
@@ -23,10 +24,18 @@ class activity(commands.Cog):
             for channel in guild.text_channels:
                 text_channel_list.append(channel.id)
             
-            print(text_channel_list)
-            print(text_channel_list[0])
+            # on vérifie que le serveur est enregistré
+            data = lire_bdd_perso(f'SELECT server_id from channels_discord', index_col='server_id').transpose()
+        
+            if not int(guild.id) in data.index:
+                requete_perso_bdd(f'''INSERT INTO public.channels_discord(
+	            server_id, id_owner, id_owner2, chan_pm, chan_tracklol, chan_accueil, chan_twitch, chan_lol, chan_tft, chan_lol_others, role_admin)
+	            VALUES (:server_id, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan, :chan);''',
+             {'server_id' : guild.id, 'chan' : text_channel_list[0]})
+
+                
             
-            print(f' Channels connectées => Name : {guild.name} | Id : {guild.id} |')
+            print(f' Channels connectées => Name : {guild.name} | Id : {guild.id} | Chan1 : {text_channel_list[0]}')
             
                
         if not main.check_for_unmute.is_running():    
