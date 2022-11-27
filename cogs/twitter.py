9980b748cbb45c2d156061965541a77af63c57b1
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-11-27 17:37:59 +0100

    Support aiohttp

--- cogs/twitter.py
+++ cogs/twitter.py
@@ -8,28 +8,54 @@ import sys
 from TwitterAPI import TwitterAPI
 from fonctions.permissions import isOwner2_slash
 from fonctions.channels_discord import chan_discord
+import oauthlib.oauth1, aiohttp
 
-
-api = TwitterAPI(os.environ.get('API_TWITTER'),
+client = oauthlib.oauth1.Client(os.environ.get('API_TWITTER'),
                  os.environ.get('SECRET_TWITTER'),
                  os.environ.get('ACCESS_TWITTER'),
-                 os.environ.get('ACCESS_SECRET_TWITTER'),
-                 api_version='2')
+                 os.environ.get('ACCESS_SECRET_TWITTER'))
 
-def get_user_id(user:str):
+async def get_user_id(user:str, session):
 
-    user_r = api.request(f'users/by/username/:{user}')
-    user_r = user_r.json()
+    uri = f'https://api.twitter.com/2/users/by/username/{user}'
+    http_method = 'GET'
+    headers = {
+        'User-Agent': 'python-TwitterAPI'}
+    
+        # Sign the request data. This needs to be called for each request you make.
+    uri,headers,body = client.sign(
+        uri = uri,
+        http_method = http_method,
+        headers = headers
+    )
+    async with session.get(uri, headers=headers) as user_twitter:
+        user_r = await user_twitter.json()
+
     user_id = user_r['data']['id']
     return user_id
 
-def get_tweet(user_id:str, num_tweet:int=0, max_results:int=10):
+async def get_tweet(user_id:str, session, num_tweet:int=0, max_results:int=10):
 
-    tweets = api.request(f'users/:{user_id}/tweets', {'max_results' : max_results})
-    id_tweet = tweets.json()['data'][num_tweet]['id']
-    contenu_tweet = tweets.json()['data'][num_tweet]['text']
+    
+    uri = f'https://api.twitter.com/2/users/{user_id}/tweets?max_results={max_results}'
+    http_method = 'GET'
+    headers = {
+        'User-Agent': 'python-TwitterAPI'}
+    
+    # Sign the request data. This needs to be called for each request you make.
+    uri,headers,body = client.sign(
+        uri = uri,
+        http_method = http_method,
+        headers = headers
+    )
+    async with session.get(uri, headers=headers) as tweets_list:
+        tweets = await tweets_list.json()
+
+    id_tweet = tweets['data'][num_tweet]['id']
+    contenu_tweet = tweets['data'][num_tweet]['text']
     
     return id_tweet, contenu_tweet
+
 
 
 
@@ -45,31 +71,31 @@ class Twitter(Extension):
     
     
     
-    @interactions.extension_command(name="last_tweet",
-                       description="Dernier tweet",
-                       options=[Option(name="pseudo",
-                                       description= "pseudo twitter",
-                                       type=interactions.OptionType.STRING,
-                                       required=True),
-                                Option(name="num_tweet",
-                                       description="numero tweet, de 0 à 5",
-                                       type=interactions.OptionType.INTEGER,
-                                       required=False,
-                                       min_value=0,
-                                       max_value=5)])
-    async def last_tweet(self, ctx:CommandContext, pseudo:str, num_tweet:int=0):
+    # @interactions.extension_command(name="last_tweet",
+    #                    description="Dernier tweet",
+    #                    options=[Option(name="pseudo",
+    #                                    description= "pseudo twitter",
+    #                                    type=interactions.OptionType.STRING,
+    #                                    required=True),
+    #                             Option(name="num_tweet",
+    #                                    description="numero tweet, de 0 à 5",
+    #                                    type=interactions.OptionType.INTEGER,
+    #                                    required=False,
+    #                                    min_value=0,
+    #                                    max_value=5)])
+    # async def last_tweet(self, ctx:CommandContext, pseudo:str, num_tweet:int=0):
         
 
-        user_id = get_user_id(pseudo)
-        ctx.defer(ephemeral=False)
+    #     user_id = get_user_id(pseudo)
+    #     ctx.defer(ephemeral=False)
         
 
-        id_tweet, msg_tweet = get_tweet(user_id, num_tweet=num_tweet)
+    #     id_tweet, msg_tweet = get_tweet(user_id, num_tweet=num_tweet)
         
  
-        url_tweet = f'https://twitter.com/{pseudo}/status/{id_tweet}'
+    #     url_tweet = f'https://twitter.com/{pseudo}/status/{id_tweet}'
         
-        await ctx.send(f'Tweet {pseudo} : ' + url_tweet)
+    #     await ctx.send(f'Tweet {pseudo} : ' + url_tweet)
         
     @interactions.extension_command(name="add_tweet",
                        description="Ajoute un twitter au tracking",
@@ -79,7 +105,9 @@ class Twitter(Extension):
                                        required=True)])
     @isOwner2_slash()
     async def add_tweet(self, ctx:CommandContext, pseudo:str):
-        user_id = get_user_id(pseudo)
+        
+        session = aiohttp.ClientSession()
+        user_id = await get_user_id(pseudo, session)
         
         requete_perso_bdd('''INSERT INTO public.twitter(
 	                    twitter, id_twitter, id_last_msg_twitter)
@@ -95,6 +123,7 @@ class Twitter(Extension):
     async def twitter_suivi(self):
         # TODO : faire par serveur
         discord_server_id = chan_discord(494217748046544906)
+        session = aiohttp.ClientSession()
 
         channel_tracklol = await interactions.get(client=self.bot,
                                                       obj=interactions.Channel,
@@ -109,11 +138,11 @@ class Twitter(Extension):
             id_last_msg = twitter['id_last_msg_twitter'] # id dernier msg
                 
             # on récupère l'id twitter
-            user_id = get_user_id(user)
+            user_id = await get_user_id(user, session)
                 
             # now on cherche les tweets
             try:
-                id_tweet, contenu_tweet = get_tweet(user_id, max_results=5)
+                id_tweet, contenu_tweet = await get_tweet(user_id, session, max_results=5)
             except KeyError: # si un tweet est supprimé, il n'y a plus de data, mais il y a toujours une trace. On passe au tweet suivant
                 continue
                 
