0d8d220ee51f75934d212ecd6f5e84edacf06ddc
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-03 02:28:57 +0100

    Cancel rembg method

--- cogs/divers.py
+++ cogs/divers.py
@@ -317,18 +317,8 @@ class Divers(Extension):
                                         description='image au format png ou jpg',
                                         type=interactions.OptionType.ATTACHMENT,
                                         required=True
-                                    ),
-                                             Option(
-                                                 name='modele',
-                                                 description='Quel modèle utilisé ?',
-                                                 type=interactions.OptionType.STRING,
-                                                 required=True,
-                                                 choices=[
-                                                    Choice(name='modèle 1', value='1'),
-                                                    Choice(name='modèle 2', value='2')
-                                                 ]
-                                             )])
-    async def remove_background(self, ctx:CommandContext, image:interactions.Attachment, modele:str):
+                                    )])
+    async def remove_background(self, ctx:CommandContext, image:interactions.Attachment):
         
         if not image.filename.endswith('.png') and not image.filename.endswith('.jpg'):
             return await ctx.send("Incompatible. Il faut une image au format png ou jpg")
@@ -343,37 +333,35 @@ class Divers(Extension):
         # load image
         img = cv2.imread('image_original.png')
         
-        if modele == '1': 
+
         # convert to graky
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # threshold input image as mask
-            mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
+        mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
 
             # negate mask
-            mask = 255 - mask
+        mask = 255 - mask
 
             # apply morphology to remove isolated extraneous noise
             # use borderconstant of black since foreground touches the edges
-            kernel = np.ones((3,3), np.uint8)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
-            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        kernel = np.ones((3,3), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 
             # anti-alias the mask -- blur then stretch
             # blur alpha channel
-            mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
+        mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
 
             # linear stretch so that 127.5 goes to 0, but 255 stays 255
-            mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
+        mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
 
             # put mask into alpha channel
-            result = img.copy()
-            result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
-            result[:, :, 3] = mask
+        result = img.copy()
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
+        result[:, :, 3] = mask
         
-        elif modele == '2':
-            pass
-            # result = remove(img)
+
 
         # save resulting masked image
         cv2.imwrite('image.png', result)
