f2823ce77e03018ca41161b985b9564a3933ac96
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-17 17:37:42 +0100

    Modify embed champion

--- cogs/leagueoflegends.py
+++ cogs/leagueoflegends.py
@@ -100,11 +100,11 @@ def records_check2(fichier,
             if methode == 'max':
                 if float(record_champion) < float(result_category_match):
                     embed = embed + \
-                        f"\n ** :rocket: Tu as battu le record sur le champion en {category.lower()} avec {result_category_match} ** (Anciennement : {record_champion})"
+                        f"\n ** :rocket: Tu as battu le record sur le champion en {category.lower()} avec {result_category_match} ** (Anciennement : {record_champion} par {joueur_champion})"
             else:
                 if float(record_champion) > float(result_category_match):
                     embed = embed + \
-                        f"\n ** :rocket: Tu as battu le record sur {champion_champion} en {category.lower()} avec {result_category_match} ** (Anciennement : {record_champion})"
+                        f"\n ** :rocket: Tu as battu le record sur {champion_champion} en {category.lower()} avec {result_category_match} ** (Anciennement : {record_champion} par {joueur_champion})"
         else:
             embed = embed + \
                 f"\n ** :rocket: Premier record sur {champion_champion} en {category} avec {result_category_match} **"
