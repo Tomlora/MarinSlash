daf5c6125778f2a89e0c85f9267e0b77998d3252
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-20 18:43:34 +0100

    Perm for staff role

--- fonctions/permissions.py
+++ fonctions/permissions.py
@@ -11,5 +11,13 @@ def isOwner_slash(ctx: CommandContext):
     id_owner2 = chan_discord_id.id_owner2
     return ctx.author.id in [id_tom, id_owner2]
 
+def isOwner_or_mod_slash(ctx: CommandContext):
+    """A utiliser pour des if dans des commandes personnalisées"""
+    chan_discord_id = chan_discord(int(ctx.guild.id))
+    id_tom = chan_discord_id.id_owner
+    id_owner2 = chan_discord_id.id_owner2
+    mod_role = chan_discord_id.role_admin
+    return (ctx.author.id in [id_tom, id_owner2] or mod_role in ctx.author.roles) 
+
 # Plus élaboré (msg général)
 
