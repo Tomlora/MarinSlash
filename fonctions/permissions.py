224562630c497ec437b0356ee58996a1a13d9d74
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-20 14:51:45 +0100

    Remove deprecated perm

--- fonctions/permissions.py
+++ fonctions/permissions.py
@@ -1,5 +1,3 @@
-
-from discord.ext import commands
 from interactions import CommandContext
 from fonctions.channels_discord import chan_discord
 
@@ -15,15 +13,3 @@ def isOwner_slash(ctx: CommandContext):
 
 # Plus élaboré (msg général)
 
-
-def isOwner2_slash():
-    """A utiliser en tant que décorateur"""
-    async def predicate(ctx: CommandContext):
-        chan_discord_id = chan_discord(int(ctx.guild.id))
-        id_tom = chan_discord_id.id_owner
-        id_owner2 = chan_discord_id.id_owner2
-        if not ctx.author.id in [id_tom, id_owner2]:
-            await ctx.send("Cette commande est réservée au propriétaire du bot")
-        return ctx.author.id in [id_tom, id_owner2]
-
-    return commands.check(predicate)
