75aeaa5ea14867d8e4a18e1f6f2836b221dd41e7
Kévin <101778157+Tomlora@users.noreply.github.com>
2022-12-27 17:36:43 +0100

    Simplification remind_call

--- cogs/divers.py
+++ cogs/divers.py
@@ -16,6 +16,8 @@ from fonctions.channels_discord import convertion_temps, mention
 from fonctions.date import heure_actuelle
 
 reminders = {}
+
+
 class Divers(Extension):
     def __init__(self, bot):
         self.bot: interactions.Client = bot
@@ -27,7 +29,7 @@ class Divers(Extension):
 
         self.task1 = create_task(IntervalTrigger(60))(self.check_for_unmute)
         self.task1.start()
-        
+
         self.task2 = create_task(IntervalTrigger(60))(self.remind_call)
         self.task2.start()
 
@@ -362,16 +364,16 @@ class Divers(Extension):
                                             required=False
                                         )])
     async def remindme_ponctuel(self,
-                       ctx: CommandContext,
-                       msg: str,
-                       time: str,
-                       public: bool = False):
-        
+                                ctx: CommandContext,
+                                msg: str,
+                                time: str,
+                                public: bool = False):
+
         try:
             duree = await convertion_temps(ctx, time)
         except:
             pass
-        
+
         if ctx.author.id not in reminders:
             reminders[ctx.author.id] = []
         reminders[ctx.author.id].append((msg, duree))
@@ -382,7 +384,7 @@ class Divers(Extension):
         else:
             await ctx.author.send(msg)
         reminders[ctx.author.id].pop(0)
-        
+
     @interactions.extension_command(name='remindme_delete_ponctuel',
                                     description="Liste des rappels ponctuels",
                                     options=[Option(
@@ -393,13 +395,12 @@ class Divers(Extension):
                                     )])
     async def list_reminders(self,
                              ctx: CommandContext,
-                             numero_rappel:int):
+                             numero_rappel: int):
         if ctx.author.id not in reminders:
             await ctx.send("Vous n'avez pas de rappels enregistrés.", ephemeral=True)
         else:
             reminders[ctx.author.id].pop(numero_rappel)
             await ctx.send('Fait !')
-
 
     @interactions.extension_command(name='remindme_list_ponctuel',
                                     description="Liste des rappels ponctuels")
@@ -412,11 +413,12 @@ class Divers(Extension):
 
                 for i, reminder in enumerate(reminders[ctx.author.id]):
                     msg, delay = reminder
-                    delay_total = current_time + datetime.timedelta(seconds=delay)
+                    delay_total = current_time + \
+                        datetime.timedelta(seconds=delay)
                     await ctx.send(f'Rappel #{i+1}: "{msg}" à {delay_total.strftime("%d/%m/%Y %H:%M:%S")} secondes', ephemeral=True)
             else:
                 await ctx.send('Tous les rappels ont été éxecutés.', ephemeral=True)
-                
+
     @interactions.extension_command(name='remindme_quotidien',
                                     description="Rappel à répéter",
                                     options=[
@@ -453,104 +455,97 @@ class Divers(Extension):
                                             description='rappel public ou en mp ?',
                                             type=interactions.OptionType.BOOLEAN,
                                             required=False)
-                                        ])
+                                    ])
     async def remindme_quotidien(self,
-                       ctx: CommandContext,
-                       msg: str,
-                       heure: int,
-                       minute: int,
-                       repetition:int,
-                       channel : interactions.Channel = None,
-                       public: bool = False):
-        
+                                 ctx: CommandContext,
+                                 msg: str,
+                                 heure: int,
+                                 minute: int,
+                                 repetition: int,
+                                 channel: interactions.Channel = None,
+                                 public: bool = False):
+
         user_id = int(ctx.author.id)
         guild_id = int(ctx.guild_id)
-        
+
         if channel == None:
             channel_id = int(ctx.channel_id)
         else:
             channel_id = int(channel.id)
-            
-        
+
         requete_perso_bdd('''INSERT INTO remind(
                             guild, channel, heure, minute, repetition, "user", public, msg)
                             VALUES (:guild_id, :channel_id, :heure, :minute, :repetition, :user_id, :public, :msg);''',
-                            {'guild_id' : guild_id,
-                             'channel_id' : channel_id,
-                             'heure' : heure,
-                             'minute' : minute,
-                             'repetition' : repetition,
-                             'user_id' : user_id,
-                             'public' : public,
-                             'msg' : msg})
-        
+                          {'guild_id': guild_id,
+                           'channel_id': channel_id,
+                           'heure': heure,
+                           'minute': minute,
+                           'repetition': repetition,
+                           'user_id': user_id,
+                           'public': public,
+                           'msg': msg})
 
         await ctx.send(f'Rappel enregistré pour {heure}:{minute}', ephemeral=True)
 
-        
     @interactions.extension_command(name='remindme_list_quotidien',
                                     description="Rappel à répéter")
     async def remindme_quotidien_list(self,
-                       ctx: CommandContext):
-        
-        
+                                      ctx: CommandContext):
+
         await ctx.defer(ephemeral=True)
-        
-        df = lire_bdd_perso('SELECT * from remind where "user" = %(user_id)s and repetition >= 1' ,
+
+        df = lire_bdd_perso('SELECT * from remind where "user" = %(user_id)s and repetition >= 1',
                             index_col='id',
-                            params={'user_id' : int(ctx.author.id)}).transpose()
-        
-        
+                            params={'user_id': int(ctx.author.id)}).transpose()
+
         if len(df) >= 1:
             msg = 'Rappels actifs : \n'
-            
+
             for id_rappel, infos_rappel in df.iterrows():
                 channel = await interactions.get(self.bot,
-                                            interactions.Channel,
-                                            object_id = infos_rappel['channel'])
+                                                 interactions.Channel,
+                                                 object_id=infos_rappel['channel'])
                 guild = await interactions.get(self.bot,
-                                            interactions.Guild,
-                                            object_id = infos_rappel['guild'])
-                
+                                               interactions.Guild,
+                                               object_id=infos_rappel['guild'])
+
                 if infos_rappel["public"]:
                     msg += f'__Rappel {id_rappel}__ prévue à **{infos_rappel["heure"]}:{infos_rappel["minute"]}** {infos_rappel["repetition"]} fois dans **{guild.name}** | Public : **Oui** : \n {infos_rappel["msg"]} \n '
                 else:
                     msg += f'__Rappel {id_rappel}__ prévue à **{infos_rappel["heure"]}:{infos_rappel["minute"]}** {infos_rappel["repetition"]} fois dans **{guild.name}** | Channel **{channel.name}** : \n {infos_rappel["msg"]} \n '
-                
-                
+
             await ctx.send(msg, ephemeral=True)
-        
+
         else:
             await ctx.send('Pas de rappel actif', ephemeral=True)
 
-
-
     async def remind_call(self):
         heure, minute = heure_actuelle()
-        df = lire_bdd_perso('SELECT * from remind where heure = %(heure)s and minute = %(minute)s and repetition >= 1' ,
+        df = lire_bdd_perso('SELECT * from remind where heure = %(heure)s and minute = %(minute)s and repetition >= 1',
                             index_col='id',
-                            params={'heure' : heure,
-                                    'minute' : minute}).transpose()
-        
-        if len(df) >= 1: # s'il y a des données
+                            params={'heure': heure,
+                                    'minute': minute}).transpose()
+
+        if len(df) >= 1:  # s'il y a des données
 
             for id_rappel, infos_rappel in df.iterrows():
-                channel = await interactions.get(self.bot,
-                                         interactions.Channel,
-                                         object_id = infos_rappel['channel'])
                 user = await interactions.get(self.bot,
-                                         interactions.User,
-                                         object_id = infos_rappel['user'])
-                
+                                              interactions.User,
+                                              object_id=infos_rappel['user'])
+
                 if infos_rappel['public']:
+
+                    channel = await interactions.get(self.bot,
+                                                     interactions.Channel,
+                                                     object_id=infos_rappel['channel'])
+
                     await channel.send(f'__Rappel__ {mention(user.id, "membre")} : {infos_rappel["msg"]} ')
                 else:
-                    await user.send(f'__Rappel__ : {infos_rappel["msg"]}') 
-                    
-                requete_perso_bdd('UPDATE remind SET repetition = repetition - 1 where id = :id ', {'id' : id_rappel})
-        
-                
-        
+                    await user.send(f'__Rappel__ : {infos_rappel["msg"]}')
+
+                requete_perso_bdd(
+                    'UPDATE remind SET repetition = repetition - 1 where id = :id ', {'id': id_rappel})
+
 
 def setup(bot):
     Divers(bot)
