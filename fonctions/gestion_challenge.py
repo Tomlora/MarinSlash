c4a150ef81d1d3cb9587e70d5d82ea9dafd39678
Kévin <101778157+Tomlora@users.noreply.github.com>
2023-04-07 18:28:30 +0200

    Add challenge best prog

--- fonctions/gestion_challenge.py
+++ fonctions/gestion_challenge.py
@@ -110,6 +110,7 @@ class challengeslol():
         self.summonerName = summonerName.lower().replace(' ', '')
         self.puuid = puuid
         self.session = session
+        self.category = ['CRISTAL', 'IMAGINATION', 'EXPERTISE', 'VÉTÉRANCE', "TRAVAIL D'ÉQUIPE"]
         
     async def preparation_data(self):
         
@@ -138,37 +139,65 @@ class challengeslol():
     async def comparaison(self):
         self.df_old_data = lire_bdd_perso(f'''SELECT * from challenges_data where "Joueur" = '{self.summonerName}' ''').transpose().sort_index().rename(columns={'value' : 'value_precedente',
                                                                                                                                 'percentile': 'percentile_precedent',
-                                                                                                                                'level' : 'level_precedent'})
+                                                                                                                                'level' : 'level_precedent',
+                                                                                                                                'position' : 'position_precedente'})
         
         if not self.df_old_data.empty:
         
-            self.data_comparaison = self.data_joueur.merge(self.df_old_data[['Joueur', 'challengeId', 'value_precedente', 'percentile_precedent', 'level_precedent']],
+            self.data_comparaison = self.data_joueur.merge(self.df_old_data[['Joueur', 'challengeId', 'value_precedente', 'percentile_precedent', 'level_precedent', 'position_precedente']],
                                                         how='left',
                                                         on=['Joueur', 'challengeId'])
             
             self.data_comparaison['new_value'] = self.data_comparaison['value'] - self.data_comparaison['value_precedente']
             self.data_comparaison['new_percentile'] = self.data_comparaison['percentile'] - self.data_comparaison['percentile_precedent']
             self.data_comparaison['new_level'] = (self.data_comparaison["level"] != self.data_comparaison["level_precedent"])
+            self.data_comparaison['new_position'] = self.data_comparaison["position_precedente"] - self.data_comparaison["position"]
             
             self.data_comparaison.sort_values('new_value', ascending=False, inplace=True)
             
             self.data_new_value = self.data_comparaison[self.data_comparaison['new_value'] > 0]
-            self.data_new_percentile = self.data_comparaison[self.data_comparaison['new_percentile'] > 0]
+            self.data_new_value['evolution'] = (self.data_new_value['value'] - self.data_new_value['value_precedente']) / self.data_new_value['value_precedente'] * 100
+            self.data_new_percentile = self.data_comparaison[(self.data_comparaison['new_percentile'] > 0.01)]
             self.data_new_level = self.data_comparaison[self.data_comparaison['new_level'] == True]
+            self.data_new_position = self.data_comparaison[self.data_comparaison['new_position'] != 0]
+            self.data_evolution = self.data_new_value.sort_values('evolution', ascending=False)
+            
+            # on ne retient pas les petites améliorations, ça n'a aucun sens
+            self.data_evolution = self.data_evolution[(self.data_evolution['evolution'] > 1) & (self.data_evolution['new_value'] > 2)]
+            
+            # on retire les categories de new_value et new_percentile
+            
+            self.data_new_value = self.data_new_value[~self.data_new_value['name'].isin(self.category)]
+            self.data_new_percentile = self.data_new_percentile[~self.data_new_percentile['name'].isin(self.category)]
+            
+            self.data_new_percentile.sort_values('new_percentile', ascending=False, inplace=True)
+            self.data_new_position.sort_values('new_position', ascending=False, inplace=True)
+            
+            
             
     
     async def embedding_discord(self, embed : Embed):
         
         txt = ''
         if not self.data_new_value.empty:
-            print('a')
+            for joueur, data in self.data_new_value.head(5).iterrows():
+                    txt += f'\n:sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu as gagné **{int(data["new_value"])}** points'
+                    
+        if not self.data_evolution.empty:
+            for joueur, data in self.data_evolution.head(5).iterrows():
+                txt += f'\n:sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu as gagné **{int(data["new_value"])}** points, soit **+{data["evolution"]:.2f}%**'
             
         if not self.data_new_percentile.empty:
-            print('a')
+            for joueur, data in self.data_new_percentile.head(5).iterrows():
+                txt += f'\n:sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu es désormais dans les **{data["new_percentile"]:.2f}%** top'
+                
+        if not self.data_new_position.empty:
+            for joueur, data in self.data_position.head(5).iterrows():
+                txt += f'\n:sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu as gagné **{int(data["new_position"])}** places'
             
         if not self.data_new_level.empty:
             for joueur, data in self.data_new_level.iterrows():
-                txt += f' :sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu es passé **{data["level"]}**'
+                txt += f'\n:sparkles: **{data["name"]}** ({data["shortDescription"]}) : Tu es passé **{int(data["level"])}**'
         
         if txt != '':    
             embed.add_field(name='Challenges', value=txt, inline=False)    
