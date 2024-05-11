6a48a2dc5abd06699135e6778e754fc99c9acdc0
Kévin <101778157+Tomlora@users.noreply.github.com>
2024-05-11 11:58:04 +0200

    Handle params with bdd

--- fonctions/params.py
+++ fonctions/params.py
@@ -1,5 +1,9 @@
-Version = 35.0
-saison = 14
-heure_aram = 4
-heure_lolsuivi = 3
-heure_challenge = 5
+from fonctions.gestion_bdd import lire_bdd_perso
+
+params = lire_bdd_perso('select * from settings', format='dict', index_col='parametres')
+
+Version = params['version']['value']
+saison = int(params['saison']['value'])
+heure_aram = int(params['heure_aram']['value'])
+heure_lolsuivi = int(params['heure_lolsuivi']['value'])
+heure_challenge = int(params['heure_challenge']['value'])
