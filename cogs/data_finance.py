25b2745da7489c154c33c1f406c058b668ca06f1
Kévin <101778157+Tomlora@users.noreply.github.com>
2024-05-26 14:21:03 +0200

    Add comparisons between tickers

--- cogs/data_finance.py
+++ cogs/data_finance.py
@@ -3,6 +3,10 @@ import interactions
 from interactions import SlashCommandOption, Extension, SlashContext, SlashCommandChoice, slash_command
 import os
 import dataframe_image as dfi
+import yfinance as yf
+import numpy as np
+import plotly_express as px
+from fonctions.channels_discord import get_embed
 
 # Activity au lancement du bot
 
@@ -52,6 +56,8 @@ class data_finance(Extension):
                         ctx: SlashContext,
                         ticker,
                         vue):
+        
+        await ctx.defer(ephemeral=False)
 
         entreprise = askFinance(ticker)
 
@@ -89,6 +95,12 @@ class data_finance(Extension):
                                           type=interactions.OptionType.STRING,
                                           required=True,
                                           choices=[
+                                                SlashCommandChoice(
+                                                  name='3 jours', value='3d'),
+                                                SlashCommandChoice(
+                                                  name='1 semaine', value='7d'),
+                                                SlashCommandChoice(
+                                                  name='2 semaines', value='14d'),
                                               SlashCommandChoice(
                                                   name='1 mois', value='1mo'),
                                               SlashCommandChoice(
@@ -103,6 +115,8 @@ class data_finance(Extension):
                         ctx: SlashContext,
                         ticker,
                         periode):
+        
+        await ctx.defer(ephemeral=False)
 
         entreprise = askFinance(ticker)
 
@@ -121,6 +135,8 @@ class data_finance(Extension):
     async def valorisation(self,
                         ctx: SlashContext,
                         ticker):
+        
+        await ctx.defer(ephemeral=False)
 
         entreprise = askFinance(ticker)
 
@@ -139,6 +155,108 @@ class data_finance(Extension):
             ctx.send("Cette méthode n'est pas adaptée aux ETF")
 
 
+    @finance.subcommand("correlation",
+                            sub_cmd_description="Corrélation entre entreprise",
+                            options=[
+                                SlashCommandOption(name="ticker",
+                                                    description="Tickers des entreprises avec un espace entre chaque : TTE.PA STLAB.PA",
+                                                    type=interactions.OptionType.STRING,
+                                                    required=True),
+                                SlashCommandOption(name="periode",
+                                          description="Periode",
+                                          type=interactions.OptionType.STRING,
+                                          required=False,
+                                          choices=[
+                                                SlashCommandChoice(
+                                                  name='3 jours', value='3d'),
+                                                SlashCommandChoice(
+                                                  name='1 semaine', value='7d'),
+                                                SlashCommandChoice(
+                                                  name='2 semaines', value='14d'),
+                                              SlashCommandChoice(
+                                                  name='1 mois', value='1mo'),
+                                              SlashCommandChoice(
+                                                  name='6 mois', value='6mo'),
+                                              SlashCommandChoice(
+                                                  name='1 an', value='1y'),
+                                              SlashCommandChoice(
+                                                  name='3 ans', value='3y'),
+                                            SlashCommandChoice(name='5 ans', value='5y'),
+                                            SlashCommandChoice(name='10 ans', value='10y')])])
+    
+    async def correlation(self,
+                        ctx: SlashContext,
+                        ticker : str,
+                        periode='max'):  
+
+      
+        liste_ticker = ticker.split(' ')  
+
+        await ctx.defer(ephemeral=False)
+
+        data = yf.download(list(liste_ticker), period=periode)
+
+        data = data['Adj Close']
+
+        fig = px.imshow(np.round(data.corr(),2), text_auto=True, aspect="auto")
+
+        del data
+
+        embed, file = get_embed(fig, name='correlation')
+
+        await ctx.send(embeds=embed, files=file)
+
+    @finance.subcommand("evolutions",
+                            sub_cmd_description="Corrélation entre entreprise",
+                            options=[
+                                SlashCommandOption(name="ticker",
+                                                    description="Tickers des entreprises avec un espace entre chaque : TTE.PA STLAB.PA",
+                                                    type=interactions.OptionType.STRING,
+                                                    required=True),
+                                SlashCommandOption(name="periode",
+                                          description="Periode",
+                                          type=interactions.OptionType.STRING,
+                                          required=False,
+                                          choices=[
+                                                SlashCommandChoice(
+                                                  name='3 jours', value='3d'),
+                                                SlashCommandChoice(
+                                                  name='1 semaine', value='7d'),
+                                                SlashCommandChoice(
+                                                  name='2 semaines', value='14d'),
+                                              SlashCommandChoice(
+                                                  name='1 mois', value='1mo'),
+                                              SlashCommandChoice(
+                                                  name='6 mois', value='6mo'),
+                                              SlashCommandChoice(
+                                                  name='1 an', value='1y'),
+                                              SlashCommandChoice(
+                                                  name='3 ans', value='3y'),
+                                            SlashCommandChoice(name='5 ans', value='5y'),
+                                            SlashCommandChoice(name='10 ans', value='10y')])])
+    async def evolutions(self,
+                        ctx: SlashContext,
+                        ticker : str,
+                        periode='max'):  
+
+      
+        liste_ticker = ticker.split(' ')  
+
+        await ctx.defer(ephemeral=False)
+
+        data = yf.download(list(liste_ticker), period=periode)
+
+        data = data['Adj Close']
+
+        data_melt = data.melt(ignore_index=False)
+
+        fig = px.line(data_melt, x=data_melt.index, y='value', color='Ticker')
+
+        embed, file = get_embed(fig, name='evolution')
+
+        del data, data_melt
+
+        await ctx.send(embeds=embed, files=file)
 
 
 
