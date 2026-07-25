8f01437add81404620d429aad9ecc1e7170da870
Kévin <101778157+Tomlora@users.noreply.github.com>
2026-07-25 21:56:33 +0200

    Add Fantasy provider package

---
+++ fonctions/fantasy/providers/__init__.py
@@ -0,0 +1,5 @@
+"""External data provider interfaces for Fantasy LoL."""
+
+from .base import PlayerProvider, ResultProvider, ScheduleProvider
+
+__all__ = ["PlayerProvider", "ResultProvider", "ScheduleProvider"]
